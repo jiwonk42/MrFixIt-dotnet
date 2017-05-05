a4d80c39dea80483872ba43211aa5a11933f270a
Jiwon Kang <jiwonk42@gmail.com>
2017-05-05 12:27:08 -0700

    make table names plural

--- src/MrFixIt/Models/Worker.cs
+++ src/MrFixIt/Models/Worker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace MrFixIt.Models
 {
+    [Table("Workers")]
     public class Worker
     {
         [Key]
